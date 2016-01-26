a2bbab3d3ced9f24c387a33a94d50ead67ff6ae8
iLuffy <xiaopohailong@outlook.com>
2016-01-26 23:26:32 +0800

    add caller member name attribute for view model base

---
+++ Halo/HaloPrinterTest/Printer/USBPrinterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ILuffy.Halo.Printer.USB;
+
+namespace ILuffy.Halo.Printer.Test
+{
+    [TestClass]
+    public class USBPrinterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            USBPrinter.TestUSBPrinterWriter();
+            //var printer = new USBPrinter(null);
+            var devices = USBPrinter.TestUSBPrinterDirectly();
+
+            Console.WriteLine(devices.Count);
+        }
+    }
+}
--- Halo/HaloWPFLib/ViewModel/ViewModelBase.cs
+++ Halo/HaloWPFLib/ViewModel/ViewModelBase.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace ILuffy.Halo.Windows.ViewModel
@@ -12,47 +13,7 @@ namespace ILuffy.Halo.Windows.ViewModel
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected string ExtractPropertyName<T>(Expression<Func<T>> propertyExpression)
-        {
-            if (propertyExpression == null)
-            {
-                throw new ArgumentNullException(nameof(propertyExpression));
-            }
-
-            var memberExpression = propertyExpression.Body as MemberExpression;
-            if (memberExpression == null)
-            {
-                throw new ArgumentException("PropertySupport_NotMemberAccessExpression_Exception", nameof(propertyExpression));
-            }
-
-            var property = memberExpression.Member as PropertyInfo;
-            if (property == null)
-            {
-                throw new ArgumentException("PropertySupport_ExpressionNotProperty_Exception", nameof(propertyExpression));
-            }
-
-            var getMethond = property.GetGetMethod(true);
-            if (getMethond.IsStatic)
-            {
-                throw new ArgumentException("PropertySupport_StaticExpression_Exception", nameof(propertyExpression));
-            }
-
-            return memberExpression.Member.Name;
-        }
-
-        protected void OnPropertyChanged<T>(Expression<Func<T>> propertyExpression)
-        {
-            var handler = PropertyChanged;
-
-            if (handler != null)
-            {
-                var propertyName = ExtractPropertyName(propertyExpression);
-
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-
-        protected void OnPropertyChanged(string propertyName)
+        protected void OnPropertyChanged([CallerMemberName] String propertyName="")
         {
             var handler = PropertyChanged;
 
